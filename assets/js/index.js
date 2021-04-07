4efda1e6c5b2ce21e6ba7f0ab735f8d3f62f19de
vlad <vladiktarasenko@gmail.com>
2021-04-07 17:53:25 +0300

    home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,18 @@
-const str = "some string i need";
+ const str = "some string i need";
 
-let capitalizeString = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
-let capitalizeStr = (str) => str.split(' ').map(capitalizeString).join(' ');
+ let capitalizeString = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
+ let capitalizeStr = (str) => str.split(' ').map(capitalizeString).join(' ');
 
-console.log(capitalizeStr(str));
+ console.log(capitalizeStr(str));
+
+
+
+const str2 = "some string i need";
+
+function capitalizeStr2(str){
+  let str2 = str.split(" ").map(str2 => {
+    return str2.charAt(0).toUpperCase() + str2.slice(1);
+  });
+  return str2.join(" ");
+}
+console.log(capitalizeStr2(str2));
