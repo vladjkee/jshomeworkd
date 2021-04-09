36226c5dd869d9f4b5d3d98506742340c565b253
vlad <vladiktarasenko@gmail.com>
2021-04-09 09:30:23 +0300

    home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,30 @@
- const str = "some string i need";
-
- let capitalizeString = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
- let capitalizeStr = (str) => str.split(' ').map(capitalizeString).join(' ');
-
- console.log(capitalizeStr(str));
-
-
-
-const str2 = "some string i need";
-
-function capitalizeStr2(str){
-  let str2 = str.split(" ").map(str2 => {
-    return str2.charAt(0).toUpperCase() + str2.slice(1);
-  });
-  return str2.join(" ");
-}
-console.log(capitalizeStr2(str2));
+class RangeValidator {
+  constructor(from, to){
+    this.from = from;
+    this.to = to;
+  }
+  set from(value){
+    if(typeof value !== 'number') throw new TypeError ('wrong type, need a number');
+    this._from = newFrom;
+  }
+  set to(newTo){
+    if(typeof value !== 'number') throw new TypeError ('wrong type, need a number');
+    this._to = newTo;
+  }
+  get from(value){
+    return this._from;
+  }
+  get to(){
+    return this._to;
+  }
+  get range(){
+    return [this._from, this._to];
+  }
+  validator(num){
+    if(num < this.from || num > this.to){
+      throw new RangeError("Number if out of range")
+    }
+    return num;
+  }
+  }
+  const validator = new RangeValidator(5,10);
