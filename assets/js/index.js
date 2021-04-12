642b14f6c8675fe35498b6199d6d50cf62c98c20
vlad <vladiktarasenko@gmail.com>
2021-04-12 21:00:03 +0300

    homework

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,26 @@
-class RangeValidator {
-  constructor(from, to){
-    this.from = from;
-    this.to = to;
+class User {
+  constructor(name, surname, ){
+    this.name = name;
+    this.surname = surname;
+
   }
-  set from(value){
-    if(typeof value !== 'number') throw new TypeError ('wrong type, need a number');
-    this._from = newFrom;
+  get fullName(){
+    return `${this.name} ${this.surname}`;
   }
-  set to(newTo){
-    if(typeof value !== 'number') throw new TypeError ('wrong type, need a number');
-    this._to = newTo;
-  }
-  get from(value){
-    return this._from;
-  }
-  get to(){
-    return this._to;
-  }
-  get range(){
-    return [this._from, this._to];
-  }
-  validator(num){
-    if(num < this.from || num > this.to){
-      throw new RangeError("Number if out of range")
-    }
-    return num;
-  }
-  }
-  const validator = new RangeValidator(5,10);
+
+}
+
+class Student  extends User{
+constructor(name, surname, ){
+  super(name, surname, );
+  this.year = year; 
+}
+getCourse(){
+  const currentYear = Date().getFullYear();
+  const course = currentYear - this.year + 1;
+  return course;
+}
+ 
+}
+
+const s = new Student('John', 'Smith', 2021);
